ba87db472b13fbf4a207d3d91c7a01f8a17301eb
brianalexandro <alexandrobrian15@gmail.com>
2016-07-18 23:46:13 +0700

    halaman depan di rapiin, samain seperti halaman home di ovs sebelumnya

--- application/views/Pages/index.php
+++ application/views/Pages/index.php
@@ -1,7 +1,33 @@
-<div class="container mt5 bg-white">
-	<div class="big-wrapper">
-		<div class="row">
-			<div class="col-sm-2"></div>
-		</div>
+<main class="bs-docs-masthead custom-top-bg" id="content" role="main">
+	<div class="container">
+		<p class="lead">Your best partner for creating websies</p>
+		<button class="btn btn-primary">Try Now</button>
 	</div>
+</main>
+
+<div class="bs-docs-featurette custom-featurette">
+  	<div class="container">
+    	<h2 class="bs-docs-featurette-title">Don't know where to start?</h2>
+    	<p class="lead">Oversign provides easy tutorial to help you using our system.</p>
+    	<button class="btn btn-primary">Take A Look</button>
+  	</div>
+</div>
+
+<div class="bs-docs-featurette custom-featurette custom-bottom-bg">
+  	<div class="container">
+	    <div class="row">
+	      	<div class="col-sm-4">
+	        	<h3>Simple Configuration</h3>
+				<p>Feel the efficiency of creating websites just by click and drop</p>
+	      	</div>
+	      	<div class="col-sm-4">
+	        	<h3>Affordable</h3>
+				<p>Get your dream website for free</p>
+	      	</div>
+	      	<div class="col-sm-4">
+	        	<h3>Efficient</h3>
+				<p>In a couple of minutes, your websites is ready to be published</p>
+	      	</div>
+	    </div>
+  	</div>
 </div>
