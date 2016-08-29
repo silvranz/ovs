dce08eb16f282f175ad1ffe2fe85fea21566a1b7
silvranz <silvranz@gmail.com>
2016-08-29 23:41:15 +0700

    upload file edit banner dan logo website user

--- assets/template/template/config/global.php
+++ assets/template/template/config/global.php
@@ -1,6 +1,6 @@
 <?php
 	//$base_url = "http://" . $_SERVER['SERVER_NAME']; -> kalo di server (udah ada konfig servername di vhost)
-	$base_url = "http://" . $_SERVER['SERVER_NAME'] ."/". explode("/",$_SERVER['REQUEST_URI'])[1]."/";
+	$base_url = "http://" . $_SERVER['SERVER_NAME'] .":". $_SERVER["SERVER_PORT"] ."/". explode("/",$_SERVER['REQUEST_URI'])[1]."/";
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
