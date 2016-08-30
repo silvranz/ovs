3171d64412a7c7548f441320fa0d32e96d53e8f7
Angela <amuliawan93@gmail.com>
2016-08-30 14:14:38 +0700

    ganti query update n insert products

--- application/controllers/InlineQueryBackendSubDomain.php
+++ application/controllers/InlineQueryBackendSubDomain.php
@@ -93,67 +93,64 @@
 
 		public function InsertProducts(){	
 			$dn = 'thelana'; //domain name
-			$param_prod_cat_id = 1; //kalo insert baru lempar null
-			$param_prod_cat_name='cat2';
+			$param_prod_cat_id = 1; //kalo insert baru lempar -1
+			$param_prod_cat_name='cat_tes_baru+lagi';
 			$user_id=$this->session->userdata("userid");
-			$param_prod_name='prod2';
-			$param_prod_desc='prod_desc2';
-			$param_prod_image='prod_image2';
+			$param_prod_name='aaaabbbbccc';
+			$param_prod_desc='aaaaabbbccc';
+			$param_prod_image='aaaaabbbbccc';
 
 			$this->db->query('use '.$dn);
-			$query = $this->db->query("IF ".$param_prod_cat_id." IS NOT NULL
-				THEN
-					SET @prod_cat_id_inserted = ".$param_prod_cat_id.";
-					
-				ELSE
-					IF EXISTS(SELECT prod_cat_name FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."')
-					THEN 
-						SET @prod_cat_id_inserted = (SELECT prod_cat_id FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."');
-					ELSE
-						INSERT INTO prod_cat(prod_cat_name, prod_cat_date, prod_cat_user_input)
-						VALUES ('".$param_prod_cat_name."', CURRENT_TIMESTAMP(), ".$user_id.");
-						SET @prod_cat_id_inserted = LAST_INSERT_ID();
-					END IF;
-				END IF;
-				INSERT INTO prod
-				(prod_name, prod_desc, prod_image, prod_cat_id, prod_date, prod_user_input)
-				VALUES
-				('".$param_prod_name."', '".$param_prod_desc."', '".$param_prod_image."', @prod_cat_id_inserted, CURRENT_TIMESTAMP(), ".$user_id.");");
+			if($param_prod_cat_id != -1)
+				$prod_cat_id_inserted = $param_prod_cat_id;
+			ELSE{
+				$check_cat = $this->db->query("SELECT COUNT(prod_cat_id) 'CountProdCat' FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."'")->result()[0];
+				if ($check_cat->CountProdCat != 0){
+				    $prod_cat_id_inserted = $this->db->query("SELECT prod_cat_id FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."'")->result()[0]->prod_cat_id;
+				} else {
+					$prod_cat_id_inserted = $this->db->query("INSERT INTO prod_cat(prod_cat_name, prod_cat_date, prod_cat_user_input)
+						VALUES ('".$param_prod_cat_name."', CURRENT_TIMESTAMP(), ".$user_id.");");
+						$prod_cat_id_inserted = $this->db->insert_id();
+				}
+			}
+			$this->db->query("INSERT INTO prod
+			(prod_name, prod_desc, prod_image, prod_cat_id, prod_date, prod_user_input)
+			VALUES
+			('".$param_prod_name."', '".$param_prod_desc."', '".$param_prod_image."', ".$prod_cat_id_inserted.", CURRENT_TIMESTAMP(), ".$user_id.");");
 			echo json_encode($query->result());
 		} 
 
 		public function UpdateProducts(){	
 			$dn = 'thelana'; //domain name
-			$prod_id=1
-			$param_prod_cat_id = 1; //kalo insert baru lempar null
+			$prod_id=1;
+			$param_prod_cat_id = 1; //kalo insert baru lempar -1
 			$param_prod_cat_name='cat2';
 			$user_id=$this->session->userdata("userid");
 			$param_prod_name='prod2';
 			$param_prod_desc='prod_desc2';
 			$param_prod_image='prod_image2';
 			$this->db->query('use '.$dn);
-			$query = $this->db->query("IF ".$param_prod_cat_id." IS NOT NULL
-				THEN
-					SET @prod_cat_id_inserted = ".$param_prod_cat_id.";
-					
-				ELSE
-					IF EXISTS(SELECT prod_cat_name FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."')
-					THEN 
-						SET @prod_cat_id_inserted = (SELECT prod_cat_id FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."');
-					ELSE
-						INSERT INTO prod_cat(prod_cat_name, prod_cat_date, prod_cat_user_input)
-						VALUES ('".$param_prod_cat_name."', CURRENT_TIMESTAMP(), ".$user_id.");
-						SET @prod_cat_id_inserted = LAST_INSERT_ID();
-					END IF;
-				END IF;
+			if($param_prod_cat_id != -1)
+				$prod_cat_id_inserted = $param_prod_cat_id;
+			ELSE{
+				$check_cat = $this->db->query("SELECT COUNT(prod_cat_id) 'CountProdCat' FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."'")->result()[0];
+				if ($check_cat->CountProdCat != 0){
+				    $prod_cat_id_inserted = $this->db->query("SELECT prod_cat_id FROM prod_cat WHERE prod_cat_name='".$param_prod_cat_name."'")->result()[0]->prod_cat_id;
+				} else {
+					$prod_cat_id_inserted = $this->db->query("INSERT INTO prod_cat(prod_cat_name, prod_cat_date, prod_cat_user_input)
+						VALUES ('".$param_prod_cat_name."', CURRENT_TIMESTAMP(), ".$user_id.");");
+						$prod_cat_id_inserted = $this->db->insert_id();
+				}
+			}
+			$this->db->query("
 				UPDATE prod SET 
 				prod_name ='".$param_prod_name."', 
 				prod_desc = '".$param_prod_desc."',
 				prod_image = '".$param_prod_image."',
-				prod_cat_id = @prod_cat_id_inserted,
+				prod_cat_id = ".$prod_cat_id_inserted.",
 				prod_date_edit=CURRENT_TIMESTAMP(),
 				prod_user_edit=".$user_id."
-				WHERE prod_id=".$prod_id;
+				WHERE prod_id=".$prod_id);
 			echo json_encode($query->result());
 		} 
 
